d435496fdc70e807f53b94ddb5b96a2fdf66da88
Pragadees-62 <prarthanapragadees62@gmail.com>
2026-07-23 15:39:18 +0530

    file

--- hourglassmaximumsum.java
+++ hourglassmaximumsum.java
@@ -9,6 +9,16 @@ public class hourglassmaximumsum {
                 arr[i][j]=sc.nextInt();
             }
         }
-        
+        System.out.print(maxHourglassSum(arr));
+    }
+    public static int maxHourglassSum(int[][] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length-2; i++) {
+            for(int j = 0; j < arr[i].length-2; j++) {
+                int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]+ arr[i + 1][j + 1]+ arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+        return maxSum;
     }
 }
